b91cedb5f038546684d098adb2b0360535dc66a5
0xFloop <floopcrypto@yahoo.com>
2023-11-16 02:24:34 -0500

    write linked list tests

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,14 +1,18 @@
 use std::fmt::Debug;
 
 #[derive(Debug)]
-struct Node<T> where 
-    T: Debug + PartialEq
+struct Node<T>
+where
+    T: Debug + PartialEq,
 {
     data: T,
     next: Option<Box<Node<T>>>,
 }
 
-struct LinkedList<T> where T:Debug + PartialEq {
+struct LinkedList<T>
+where
+    T: Debug + PartialEq,
+{
     head: Option<Box<Node<T>>>,
 }
 impl<T: Debug + PartialEq> LinkedList<T> {
@@ -95,7 +99,7 @@ impl<T: Debug + PartialEq> LinkedList<T> {
     fn includes(&self, data: T) -> Result<bool, &'static str> {
         let mut current = &self.head;
 
-        while let Some(node) = current { 
+        while let Some(node) = current {
             if node.data == data {
                 return Ok(true);
             }
@@ -129,18 +133,118 @@ pub fn try_linked_list() -> Result<(), &'static str> {
 
     print_linked_list(&linked_list);
 
-    println!("Should be false: {}",linked_list.includes(99)?);
+    println!("Should be false: {}", linked_list.includes(99)?);
 
-    println!("Should be true: {}",linked_list.includes(8)?);
+    println!("Should be true: {}", linked_list.includes(8)?);
 
     return Ok(());
 }
 
-fn print_linked_list<T: Debug+PartialEq>(list: &LinkedList<T>) {
+fn print_linked_list<T: Debug + PartialEq>(list: &LinkedList<T>) {
     let mut current = &list.head;
 
     while let Some(node) = current {
         println!("{:?}", &node.data);
         current = &node.next;
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_to_linked_list() {
+        let mut linked_list = LinkedList::<u32>::new();
+
+        for i in (0..10).rev() {
+            linked_list.push(i);
+        } 
+        print_linked_list(&linked_list);
+        let mut current_node = &linked_list.head;
+        let mut i = 0;
+        while let Some(node) = current_node {
+            assert_eq!(i, node.data);
+            current_node = &node.next;
+            i+=1;
+        }
+    }    
+    #[test]
+    fn test_peek(){
+        let mut linked_list = LinkedList::<u32>::new();
+        linked_list.push(1919);
+        let head_node_value = linked_list.peek().unwrap();
+        assert_eq!(*head_node_value, 1919);
+
+    }
+    #[test]
+    fn test_pop() {
+        let mut linked_list = LinkedList::<&str>::new();
+        linked_list.push("deez");
+        linked_list.push("nuts");
+
+        let popped_value   = linked_list.pop().unwrap();
+        assert_eq!(popped_value,"nuts");
+    }
+    #[test]
+    fn test_insert(){
+        let mut linked_list = LinkedList::<u32>::new();
+        for i in 0..10{
+            linked_list.push(i);
+        }
+        let insert_index = 4;
+        let insert_data = 99;
+
+        match linked_list.insert(insert_index,insert_data){
+            Ok(()) => (),
+            Err(err) => panic!("insert test failed: {}",err)
+        }
+
+        let mut current= &linked_list.head;
+        let mut i = 1;//start at one to acount for the head
+
+        while let Some(node) = current {
+            println!("current index: {}, current value: {}",i, node.data);
+            if i == insert_index{
+                assert_eq!(node.data,insert_data)
+            }
+            current = &node.next;
+            i+=1;
+        }
+
+    }
+    #[test]
+    fn test_delete(){
+        let mut linked_list = LinkedList::<u32>::new();
+        for i in (0..10).rev(){
+            linked_list.push(i);
+        }
+        let delete_index = 6;
+
+        match linked_list.delete(delete_index){
+            Ok(()) => (),
+            Err(err) => panic!("{}",err)
+        }
+
+        let mut current= &linked_list.head;
+        let mut i = 1;//start at one to acount for the head
+
+        while let Some(node) = current {
+            println!("current index: {}, current value: {}",i, node.data);
+
+            if i == delete_index{
+                assert_eq!(node.data,i as u32);
+            }
+            current = &node.next;
+            i+=1;
+        }
+    }
+    #[test]
+    fn test_includes(){
+        let mut linked_list = LinkedList::<u32>::new();
+        for i in 0..100 {
+            linked_list.push(i);
+        }
+        assert!(linked_list.includes(8).unwrap());
+        assert!(!linked_list.includes(1999).unwrap());
     }
 }
